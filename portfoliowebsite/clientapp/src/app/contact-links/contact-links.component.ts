c7cd329d3c16295dd4baa6eb77f7568b9c61a828
Nathan Russell <mail@naterussell.com>
2019-06-11 01:22:30 -0400

    Add routing and components for primary pages.

---
+++ portfoliowebsite/clientapp/src/app/contact-links/contact-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-links',
+  templateUrl: './contact-links.component.html',
+  styleUrls: ['./contact-links.component.less']
+})
+export class ContactLinksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
