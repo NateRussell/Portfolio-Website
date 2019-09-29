4bf2c13bb53b3218827743ede8dc40a72767e3c8
Nathan Russell <mail@naterussell.com>
2019-09-29 01:17:15 -0400

    Check to see if all images have loaded before displaying the page.

--- PortfolioWebsite/ClientApp/src/app/layout-components/inner-layout/inner-layout.component.ts
+++ PortfolioWebsite/ClientApp/src/app/layout-components/inner-layout/inner-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { fadeInAnimation } from '../../page-components/content-enter-animations';
+import { debug } from 'util';
 
 @Component({
   selector: 'app-inner-layout',
@@ -13,21 +14,24 @@ export class InnerLayoutComponent implements OnInit {
   public pageReady: boolean = false; //used as trigger to coordinate new page elements to enter the page
   private topPadding: string = '10px';
 
-  @HostBinding('style.opacity') private hostOpacity: string = '0';
+  @HostBinding('style.opacity') public hostOpacity: string = '0';
 
   constructor() { }
 
   ngOnInit() {
     this.adjustTopPadding();
 
+    const DELAY: number = 600;
     setTimeout(() => { //wait until old outlet has faded away
-      window.scrollTo({ top: 0 }); //reset scroll bar to top of page
-      this.hostOpacity = '1';
-      this.pageReady = true; //trigger animation new page elements to animate in
-    }, 600);
+      this.waitForImages(() => this.setPage(this));
+    }, DELAY);
   }
 
-
+  private setPage(component: InnerLayoutComponent): void {
+    window.scrollTo({ top: 0 }); //reset scroll bar to top of page
+    component.hostOpacity = '1';
+    component.pageReady = true; //trigger animation new page elements to animate in
+  }
 
   private adjustTopPadding(): void {
 
@@ -43,6 +47,25 @@ export class InnerLayoutComponent implements OnInit {
       let totalTopPadding: string = `calc(${adjustmentHeight} + ${this.topPadding})`; //calculate the total top padding value by adding the adjustment height to the top padding height
       contentElement.style.paddingTop = totalTopPadding; //set the content element's top padding value to the calculated total top padding value
     }
+  }
+
+  private waitForImages(callback: Function): void {
+    let images: HTMLCollectionOf<HTMLImageElement> = document.images; //collection of all images on the page
+    let imageLoadPromises: Promise<boolean>[] = [];
+
+    for (let i = 0; i < images.length; i++) { //for each image
+      let promise: Promise<boolean> = new Promise<boolean>((resolve) => { //create a new promise
+        images.item(i).onload = () => resolve(true); //resolve the promise when the image loads
+        images.item(i).onerror = () => resolve(false); //resolve promise when the image fails to load
+        if (images.item(i).complete) { //resolve promise if image has already loaded
+          resolve(true);
+        }
+      });
+      imageLoadPromises.push(promise);
+    }
+    Promise.all(imageLoadPromises).then(() => { //once all images promises have resolved due to loading or failing to load
+      callback(); //trigger the callback
+    })
   }
 
 }
